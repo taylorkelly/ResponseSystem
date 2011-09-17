2462fd49ac52441f9bb714b23d78a89f75da3b9c
Taylor Kelly <tkelly910@gmail.com>
2011-09-16 23:11:26 -0400

    Initial Commit!

---
+++ src/response/client/ResponseClient.java
@@ -0,0 +1,12 @@
+package response.client;
+
+/**
+ *
+ * @author taylor
+ */
+public class ResponseClient {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/response/server/ResponseServer.java
@@ -0,0 +1,12 @@
+package response.server;
+
+/**
+ *
+ * @author taylor
+ */
+public class ResponseServer {
+    public static void main(String[] args) {
+
+    }
+
+}
